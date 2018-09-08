c928fe830134762abe308b1a21f24db6f60c6edb
Elliot <elliot@pourmand.com>
2018-09-07 18:39:21 -0700

    update tests to str

--- test/test_checkmate.py
+++ test/test_checkmate.py
@@ -12,7 +12,7 @@ def test_white_checkmates():
         "xoooooooox" + \
         "xoRoKoooox" + \
         "x" * 20
-    state = [one_from_mate_board, 0, -1, 0, 0, [0, 0, 0, 0],
+    state = [one_from_mate_board, 0, -1, 0, 0, "0000",
              one_from_mate_board.index('K'), one_from_mate_board.index('k')]
     move = best_move(state, 3, randomness=False)
     assert sq120_RF(move[0]) == 'B1'
